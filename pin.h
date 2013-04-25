efdab99c05d2d5d302cdf1451cd87003d730e7fc
Hermann Kraus <hermr2d2@gmail.com>
2013-04-26 01:12:47 +0200

    Update DigitalOut.

--- pin.h
+++ pin.h
@@ -8,14 +8,12 @@
 class DigitalOut
 {
 public:
-    explicit DigitalOut(volatile uint8_t *port, uint8_t pin, bool init_=false) :
+    explicit DigitalOut(volatile uint8_t *port, uint8_t pin, bool init=false) :
         pin_(_BV(pin)),
         port_(port)
-    { if (init_) *(port_ - 1) |= _BV(pin); }
-//    void low() const { FIOCLR = bitmask_;}
-//    void high() const { FIOSET = bitmask_;}
+    { if (init) *(port_ - 1) |= _BV(pin); }
     force_inline void operator=(int value) const { if (value) *port_ |= pin_; else *port_ &= ~pin_; }
-//    force_inline operator int() const { return FIOPIN & bitmask_ ? 1 : 0; }
+    force_inline operator int() const { return *port_ & pin_; }
 private:
     const uint8_t pin_;
     volatile uint8_t * const port_;
