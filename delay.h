034ba7cfca081451905501f0a55f9cbf8d7c099e
Hermann Kraus <hermr2d2@gmail.com>
2013-04-27 23:09:16 +0200

    Add nop() command.

--- delay.h
+++ delay.h
@@ -1,6 +1,12 @@
 #ifndef DELAY_H
 #define DELAY_H
+
+#include "config.h"
 #include <util/delay.h>
+#include <avr/cpufunc.h>
+
 #define delay_us(x) _delay_us(x)
+#define delay_ms(x) _delay_ms(x)
+#define nop() _NOP()
 
 #endif // DELAY_H
